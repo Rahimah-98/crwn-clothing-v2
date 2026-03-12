7341b344ce32e0e8880f3e4c89aeddc81c00ac4f
Rahimah Ansari <rahimaansari98@gmail.com>
2026-03-13 03:10:30 +0430

    just for test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 const App = () => {
   return (
     <div className='categories-container'>
+      {/* Test */}
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Test</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
       <div className='category-container'>
         {/* <img /> */}
         <div className='category-body-container'>
